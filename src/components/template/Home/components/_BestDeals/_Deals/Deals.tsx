cc5532fca0dc5d55d1fe518e03c0e3edc22911cc
AshkanZj <ashkanzojaji19@gmail.com>
2024-07-02 18:22:50 +0330

    fix: some ui fix in home page

--- src/components/template/Home/components/_BestDeals/_Deals/Deals.tsx
+++ src/components/template/Home/components/_BestDeals/_Deals/Deals.tsx
@@ -1,8 +1,8 @@
-import { useGetProductById } from '@/src/api/product/product.queries';
-import { Box, Button, Stack, Typography } from '@mui/material';
-import Link from 'next/link';
-import Image from 'next/image';
-import ProductRating from '../Product/components/_ProductRating/ProductRating';
+import { useGetProductById } from "@/src/api/product/product.queries";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import Image from "next/image";
+import ProductRating from "@/src/components/template/Product/components/_ProductRating/ProductRating";
 
 const DealsTemplate = ({ productId }: any) => {
   const { data, isLoading, isError } = useGetProductById(productId);
@@ -18,20 +18,20 @@ const DealsTemplate = ({ productId }: any) => {
           height={350}
           minHeight="200px"
           spacing={1}
-          justifyContent={'space-between'}
+          justifyContent={"space-between"}
         >
           <Box height={150} position="relative">
             <Image
-              style={{ borderRadius: 6, objectFit: 'contain' }}
+              style={{ borderRadius: 6, objectFit: "contain" }}
               src={data?.image}
               alt={data?.name}
               fill
-              // objectFit="cover"
-            ></Image>
+              objectFit="cover"
+            />
           </Box>
-          <Stack sx={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Typography sx={{ fontWeight: '800' }}>{data.name}</Typography>
-            <Typography sx={{ fontWeight: '800' }}>{data.price}.00$</Typography>
+          <Stack sx={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <Typography sx={{ fontWeight: "800" }}>{data.name}</Typography>
+            <Typography sx={{ fontWeight: "800" }}>{data.price}.00$</Typography>
           </Stack>
           <ProductRating />
           <Link href={`category/${data?.category}/${data?.id}`}>
@@ -43,10 +43,10 @@ const DealsTemplate = ({ productId }: any) => {
                 px: 1,
                 width: 200,
                 borderRadius: 8,
-                borderColor: 'black',
+                borderColor: "black",
                 borderWidth: 1,
-                bgcolor: 'white',
-                color: 'black',
+                bgcolor: "white",
+                color: "black",
               }}
               type="submit"
             >
